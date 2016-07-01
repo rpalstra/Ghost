0dbbb1ae96f421ddd868f04b2e441de6b523f518
Riemer Palstra <palstra@textkernel.nl>
2016-07-01 15:17:15 +0200

    Change configuration for OpenShift support.

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://ghost.palstra.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -22,8 +22,8 @@ config = {
         },
 
         server: {
-            host: '127.0.0.1',
-            port: '2368'
+            host: '0.0.0.0',
+            port: '8080'
         }
     },
 
@@ -61,9 +61,9 @@ config = {
         // Can be host & port (default), or socket
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '8080'
         },
         // #### Paths
         // Specify where your content directory lives
